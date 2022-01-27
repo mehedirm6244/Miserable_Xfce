80682e613c151e198dd423c44f0edf7dc7b41b97
Mehedi Rahman Mahi <mehedirm6244@protonmail.com>
2022-01-27 16:59:23 +0600

    Update

--- local/share/icons/Numix-Apps/update_cache.sh
+++
@@ -1,9 +0,0 @@
-### Auto update the iconcache
-### Do a manual update with following code in a terminal if you get errors
-### Do check if you have used spaces in the naming of files or folders
-### Sometimes you make a copy of an icon - this contains a space
-### gtk-update-icon-cache -f iconthemefolder
-
-### in some of the distributions you will a message popup if an icon theme has no cache
-### if you have changed the icons - deleted folders etcetera then run this script
-gtk-update-icon-cache -f ../$(basename `pwd`)
